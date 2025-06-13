b76ee25fa931cfb5382cac8e1f86b236df202a2f
geysonmejiacastro <geysonmejiacastro@gmail.com>
2025-06-13 18:14:31 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      Welcome to my profile<br></br> pick a Industry to see my work 
+    </div>
+
+
+
+
+
+  )
+}
+
+export default App
